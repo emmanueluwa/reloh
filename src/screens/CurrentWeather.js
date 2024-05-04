0a4dded9588019e464cc5b04e0d360f6c9bf756c
emmanueluwa <emmanuel@fulodev.com>
2024-05-04 11:48:50 +0100

    refactoring current weather component

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -2,24 +2,42 @@ import React from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import SafeViewAndroid from '../components/SafeViewAndroid'
 import { Feather } from '@expo/vector-icons'
+import RowText from '../components/RowText'
 
 const CurrentWeather = () => {
+  const {
+    container,
+    temp,
+    feels,
+    highLowWrapper,
+    highLow,
+    bodyWrapper,
+    description,
+    message
+  } = styles
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <View style={styles.container}>
+      <View style={container}>
         <Feather name="sun" size={100} color="black" />
-        <Text style={styles.temp}>11</Text>
-        <Text style={styles.feels}>Feels like 8</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 13</Text>
-          <Text style={styles.highLow}>Low: 11</Text>
-        </View>
+        <Text style={temp}>11</Text>
+        <Text style={feels}>Feels like 8</Text>
+
+        <RowText
+          containerStyles={highLowWrapper}
+          messageOne={'High: 13'}
+          messageTwo={'Low: 11'}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
+        />
       </View>
 
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Its sunny with light breeze</Text>
-        <Text style={styles.message}>You do not need a jacket</Text>
-      </View>
+      <RowText
+        containerStyles={bodyWrapper}
+        messageOne={'Its sunny with light breeze'}
+        messageTwo={'You do not need a jacket'}
+        messageOneStyles={description}
+        messageTwoStyles={message}
+      />
     </SafeAreaView>
   )
 }
