e24cad0992efec8ca062681a3112d141db9a5022
emmanueluwa <emmanuel@fulodev.com>
2024-05-05 08:54:33 +0100

    adding ability to get dynamic data for upcoming weather component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -36,7 +36,6 @@ const Tabs = ({ weather }) => {
       </Tab.Screen>
       <Tab.Screen
         name={'Upcoming'}
-        component={UpcomingWeather}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
@@ -46,7 +45,9 @@ const Tabs = ({ weather }) => {
             />
           )
         }}
-      />
+      >
+        {() => <UpcomingWeather weatherData={weather.list} />}
+      </Tab.Screen>
       <Tab.Screen
         name={'City'}
         component={City}
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   FlatList,
   StatusBar,
   ImageBackground
@@ -10,47 +9,7 @@ import {
 import SafeViewAndroid from '../components/SafeViewAndroid'
 import ListItem from '../components/ListItem'
 
-const DATA = [
-  {
-    dt_txt: '2022-08-30 16:00:00',
-    main: {
-      temp_min: 8.34,
-      temp_max: 7.24
-    },
-    weather: [
-      {
-        main: 'Rain'
-      }
-    ]
-  },
-  {
-    dt_txt: '2022-09-03 15:00:00',
-
-    main: {
-      temp_min: 8.2,
-      temp_max: 7.31
-    },
-    weather: [
-      {
-        main: 'Clouds'
-      }
-    ]
-  },
-  {
-    dt_txt: '2022-08-30 18:00:00',
-    main: {
-      temp_min: 8.84,
-      temp_max: 7.94
-    },
-    weather: [
-      {
-        main: 'Rain'
-      }
-    ]
-  }
-]
-
-const UpcomingWeather = () => {
+const UpcomingWeather = ({ weatherData }) => {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -67,9 +26,8 @@ const UpcomingWeather = () => {
         source={require('../../assets/up-background.jpg')}
         style={img}
       >
-        <Text>Upcoming Weather</Text>
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
